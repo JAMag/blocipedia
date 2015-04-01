22753d6565c0539ad28ea9e788c3754384f32e63
Justin Maguire <maguirejustin@hotmail.com>
2015-03-31 21:30:50 -0400

    authentication-continued-chekpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :flash_attack
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def hello
     render text: "<h1>Hello</h1><p>Welcome home</p>"
+  end
+
+  def flash_attack
+    flash[:notice] = "flashy"
+    flash[:error] = "not flashy"
   end
 end
