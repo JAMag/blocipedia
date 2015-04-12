44b46a29ed9f2806d2ada2fbfbb401b0dcbe6c28
Justin Maguire <maguirejustin@hotmail.com>
2015-04-12 19:11:29 -0400

    upgrading an account

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'stripe'
 gem 'faker'
 gem 'pundit'
 gem 'figaro', '1.0'
---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,35 @@
+Rails.logger.info "@@@@@@@@@@@@@@@@@@@@@@@ The Stripe Key: #{Rails.configuration.stripe[:publishable_key]}"
+class ChargesController < ApplicationController
+
+  def create
+    @amount = params[:amount]
+
+    customer = Stripe::Customer.create(
+    email: current_user.email,
+    card: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+    customer: customer.id,
+    amount: Amount.default,
+    description: "BigMoney Membership - #{current_user.email}",
+    currency: 'usd'
+    )
+
+    flash[:success] = "Thanks for all the money, #{current_user.email}! Give me more."
+    redirect_to user_path(current_user)
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+  end
+
+  def new
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "BigMoney Membership - #{current_user.name}",
+     
+    }
+  end
+
+end
